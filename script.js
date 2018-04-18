f4181609f4721601c0a97fbddeed9013fb2cef80
apprenant <you@example.com>
2018-04-18 10:04:25 +0200

    First git for project API key

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // Le code doit être ecrit içi
+  const apiKEY = '31c22de58047ef2e6c381f7f424f1365';
+
+  $('#a').on('click', function() {
+    $.get('https://api-2445582011268.apicast.io/games/11', function(response) {
+      debugger;
+      
+    });
+  });
+});
